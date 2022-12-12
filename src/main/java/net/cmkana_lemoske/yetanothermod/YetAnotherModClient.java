849cfd26da8fdf0a173c923598fa4504984149fa
Евгений Канашкин <evgenijkanaskin@Air-Evgenij.IGD_Rostelecom>
2022-12-12 19:41:29 +1000

    Initial commit

---
+++ src/main/java/net/cmkana_lemoske/yetanothermod/YetAnotherModClient.java
@@ -0,0 +1,10 @@
+package net.cmkana_lemoske.yetanothermod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YetAnotherModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
