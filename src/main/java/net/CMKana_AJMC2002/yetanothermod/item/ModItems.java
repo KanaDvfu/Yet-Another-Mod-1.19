6c73763f34f9e766ba373aa09a2dc3a59fdc2c26
evgenijkanaskin <kanashkin.ed@students.dvfu.ru>
2022-12-26 05:09:23 +1000

    added Sarofang

--- src/main/java/net/CMKana_AJMC2002/yetanothermod/item/ModItems.java
+++ src/main/java/net/CMKana_AJMC2002/yetanothermod/item/ModItems.java
@@ -5,7 +5,9 @@ import net.CMKana_AJMC2002.yetanothermod.YetAnotherMod;
 import net.CMKana_AJMC2002.yetanothermod.item.custom.CernosItem;
 import net.CMKana_AJMC2002.yetanothermod.item.custom.d20Item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.AxeItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -20,6 +22,9 @@ public class ModItems {
 
     public static final Item Cernos = registerItem(
             "cernos", new CernosItem(new Item.Settings().maxCount(1)));
+
+    public static final Item Sarofang = registerItem(
+            "sarofang", new AxeItem(ToolMaterials.NETHERITE,6f,-2f, new Item.Settings().maxCount(1)));
 
     public static final Item D20 = registerItem(
             "d20", new d20Item(new Item.Settings().maxCount(1)));
