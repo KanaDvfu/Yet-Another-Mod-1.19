e94a832679d5e10e5c3fe26a82efd7428747ef7b
evgenijkanaskin <kanashkin.ed@students.dvfu.ru>
2022-12-26 13:41:02 +1000

    added WorldPreset.java

--- src/main/java/net/CMKana_AJMC2002/yetanothermod/worldpreset/WorldPreset.java
+++ src/main/java/net/CMKana_AJMC2002/yetanothermod/worldpreset/WorldPreset.java
@@ -1,14 +1,29 @@
 package net.CMKana_AJMC2002.yetanothermod.worldpreset;
 
+import net.minecraft.registry.BuiltinRegistries;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.dimension.DimensionOptions;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.chunk.FlatChunkGenerator;
+import net.minecraft.world.gen.chunk.FlatChunkGeneratorConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import net.minecraft.world.gen.WorldPresets;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Optional;
 
 @Mixin(WorldPresets.Registrar.class)
 public abstract class WorldPreset {
     // defining our registry key. this key provides an Identifier for our preset, that we can use for our lang files and data elements.
     private static final RegistryKey<WorldPreset> VOID_WORLD = RegistryKey.of(Registry.WORLD_PRESET_KEY, new Identifier("wiki-example", "void_world"));
     @Shadow protected abstract RegistryEntry<WorldPreset> register(RegistryKey<WorldPreset> key, DimensionOptions dimensionOptions);
-    @Shadow protected abstract DimensionOptions createOverworldOptions(ChunkGenerator chunkGenerator);
+    @Shadow
+    protected abstract DimensionOptions createOverworldOptions(ChunkGenerator chunkGenerator);
  
     @Inject(method = "initAndGetDefault", at = @At("RETURN"))
     private void addPresets(CallbackInfoReturnable<RegistryEntry<WorldPreset>> cir) {
@@ -21,7 +36,7 @@ public abstract class WorldPreset {
                         new FlatChunkGeneratorConfig(
                                 // we don't need to overwrite the structure set
                                 Optional.empty(),
-                                BuiltinRegistries.BIOME)
+                                BuiltinRegistries.)
                         )
                 )
         );
