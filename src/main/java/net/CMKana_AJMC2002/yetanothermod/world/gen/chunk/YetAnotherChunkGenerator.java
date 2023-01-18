ea4c6d5ab2bf71783349a127b87d9b6ac7b57227
AJMC2002 <adrianmosquera2015@gmail.com>
2023-01-18 15:28:43 +1000

    Room gen is done

--- src/main/java/net/CMKana_AJMC2002/yetanothermod/world/gen/chunk/YetAnotherChunkGenerator.java
+++ src/main/java/net/CMKana_AJMC2002/yetanothermod/world/gen/chunk/YetAnotherChunkGenerator.java
@@ -7,9 +7,10 @@ import java.util.concurrent.Executor;
 import com.google.common.collect.Sets;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.CMKana_AJMC2002.yetanothermod.block.YetAnotherBlocks;
 import net.minecraft.SharedConstants;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ChunkRegion;
@@ -32,13 +33,10 @@ public class YetAnotherChunkGenerator extends ChunkGenerator {
 					.group(BiomeSource.CODEC.fieldOf("biome_source")
 							.forGetter(YetAnotherChunkGenerator::getBiomeSource))
 					.apply(instance, YetAnotherChunkGenerator::new));
-
-	private static final int NUM_FLOORS = 9;
-	private static final int FLOOR_HEIGHT = 8;
-	private static final int FLOOR_WIDTH = 16; // same for x and z
+// same for x and z
 	private static final int SEA_LEVEL = 0;
 	private static final int MINIMUM_Y = SEA_LEVEL - 32;
-	private static final int WORLD_HEIGHT = SEA_LEVEL + NUM_FLOORS * FLOOR_HEIGHT - MINIMUM_Y;
+	private static final int WORLD_HEIGHT = SEA_LEVEL + YetAnotherRooms.NUM_FLOORS * YetAnotherRooms.ROOM_HEIGHT - MINIMUM_Y;
 
 	public YetAnotherChunkGenerator(BiomeSource biomeSource) {
 		super(biomeSource);
@@ -74,21 +72,21 @@ public class YetAnotherChunkGenerator extends ChunkGenerator {
 	@Override
 	public int getHeight(int x, int z, Heightmap.Type heightmap, HeightLimitView world,
 			NoiseConfig noiseConfig) {
-		return 16;
+		return WORLD_HEIGHT;
 	}
 
 	@Override
 	public VerticalBlockSample getColumnSample(int x, int z, HeightLimitView world,
 			NoiseConfig noiseConfig) {
-		// BlockState list[] = new BlockState[1];
-		// list = Collections.nCopies(1, this.DEFAULT_BLOCK).toArray(list);
-		// return new VerticalBlockSample(16, list);
-		return null;
+		BlockState[] blockStates = new BlockState[WORLD_HEIGHT];
+		for (int y = MINIMUM_Y; y < MINIMUM_Y + WORLD_HEIGHT + 1; y++) {
+			blockStates[y - MINIMUM_Y] = this.getBlock(new BlockPos(x, y, z)).getDefaultState();
+		}
+		return new VerticalBlockSample(MINIMUM_Y, blockStates);
 	}
 
 	@Override
-	public void getDebugHudText(List<String> text, NoiseConfig noiseConfig, BlockPos pos) {
-	}
+	public void getDebugHudText(List<String> text, NoiseConfig noiseConfig, BlockPos pos) {}
 
 	@Override
 	protected Codec<? extends ChunkGenerator> getCodec() {
@@ -129,9 +127,9 @@ public class YetAnotherChunkGenerator extends ChunkGenerator {
 				mutablePos.setZ(z + zOffset);
 				for (int y = MINIMUM_Y; y < MINIMUM_Y + WORLD_HEIGHT + 1; y++) {
 					mutablePos.setY(y);
-					BlockState blockState = genBlockState(mutablePos.toImmutable());
+					BlockState blockState = getBlock(mutablePos.toImmutable()).getDefaultState();
 
-					if (blockState == YetAnotherWorldBlock.AIR.getBlockState()
+					if (blockState == Blocks.AIR.getDefaultState()
 							|| SharedConstants.isOutsideGenerationArea(chunk.getPos()))
 						continue;
 
@@ -152,24 +150,32 @@ public class YetAnotherChunkGenerator extends ChunkGenerator {
 		return chunk;
 	}
 
-	private final BlockState genBlockState(BlockPos blockPos) {
+	private final Block getBlock(BlockPos blockPos) {
 		final int x = blockPos.getX();
 		final int y = blockPos.getY();
 		final int z = blockPos.getZ();
-		if (x % FLOOR_WIDTH == 0 && y % FLOOR_HEIGHT == 0 && z % FLOOR_WIDTH == 0
-				&& y >= SEA_LEVEL) {
-			// if (y >= FLOOR_HEIGHT * 2)
-			// return YetAnotherWorldBlock.FLOOR3.getBlockState();
-			// if (y >= FLOOR_HEIGHT * 1)
-			// return YetAnotherWorldBlock.FLOOR2.getBlockState();
-			// if (y >= SEA_LEVEL)
-			// return YetAnotherWorldBlock.FLOOR1.getBlockState();
-			if (y / FLOOR_HEIGHT % 2 == 0)
-				return YetAnotherBlocks.ADRIANIUM_BLOCK.getDefaultState();
-			return YetAnotherBlocks.EUGENIUM_BLOCK.getDefaultState();
+		final int relX = Math.floorMod(x, YetAnotherRooms.ROOM_WIDTH);
+		final int relY = Math.floorMod(y, YetAnotherRooms.ROOM_HEIGHT);
+		final int relZ = Math.floorMod(z, YetAnotherRooms.ROOM_WIDTH);
+		if (y >= SEA_LEVEL) {
+			// Floor
+			if (relY == 0) {
+				if (relX == 3 && relZ == 3)
+					return Blocks.SEA_LANTERN;
+				if (relX == 3 || relZ == 3)
+					return Blocks.DEEPSLATE_TILES;
+				return Blocks.SMOOTH_STONE;
+			}
+			// Ceiling
+			if (relY == YetAnotherRooms.ROOM_HEIGHT - 1)
+				return Blocks.IRON_BLOCK;
+			// Room
+			final boolean[][] room = YetAnotherRooms.getRoom(x, y, z);
+			if (room[relX][relZ])
+				return Blocks.POLISHED_DIORITE;
+			// Air
+			return Blocks.AIR;
 		}
-		if (y <= SEA_LEVEL)
-			return YetAnotherWorldBlock.WATER.getBlockState();
-		return YetAnotherWorldBlock.AIR.getBlockState();
+		return Blocks.WATER;
 	}
 }
